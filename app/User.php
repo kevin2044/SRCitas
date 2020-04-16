ef36242587ce6ff962b1d3811738392a02755ed7
Kevin Brayan Luna Figueroa <kevin_2044@hotmail.com>
2020-04-15 18:51:48 -0600

    Agregando JWT a la aplicación

--- app/User.php
+++ app/User.php
@@ -5,8 +5,10 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -48,4 +50,12 @@ class User extends Authenticatable
     public function scopeDoctor($query){
         return $query->where('role', 'doctor');
     }
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
 }
